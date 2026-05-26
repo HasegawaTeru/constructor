331fd5192895a47efd8f04c0452b6042279030b8
4D-12\koyo <a>
2026-05-26 13:29:37 +0900

    プロジェクト ファイルを追加します。

---
+++ constructor/constructor.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+class Player
+{
+public:
+	int HP;
+
+	Player(int hp) : HP(hp)
+	{
+
+	}
+};
+
+int main()
+{
+	Player player(100);
+
+	std::cout << "HP : " << player.HP;
+}
