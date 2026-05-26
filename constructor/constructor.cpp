e847bc8aa23357205037aed2182991c0ebe5af48
4D-12\koyo <a>
2026-05-26 14:57:50 +0900

    構造体を利用して結果を出力

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -17,29 +17,15 @@ class Player
 {
 public:
 	std::string Name;
-	int HP;
-	int MP;
-	int ATK;
-	int DEF;
-	int SPD;
-
-	Player()
-		: Player("Default")
-	{
-	}
-
-	Player(std::string name, int hp = 100, int mp = 0, int atk = 1, int def = 1, int spd = 1)
-		: Name(name), HP(hp), MP(mp), ATK(atk), DEF(def), SPD(spd)
-	{
-	}
+	PlayerSettings Settings;
 
 	Player(const Player& other)
-		: Name(other.Name), HP(other.HP), MP(other.MP), ATK(other.ATK), DEF(other.DEF), SPD(other.SPD)
+		: Player(other.Name, other.Settings)
 	{
 	}
 
-	Player(std::string name, const PlayerSettings& setting)
-		: Player(name, setting.HP, setting.MP, setting.ATK, setting.DEF, setting.SPD)
+	Player(std::string name, const PlayerSettings& settings)
+		: Name(name), Settings(settings)
 	{
 	}
 };
@@ -47,21 +33,17 @@ public:
 void Show(const Player& player)
 {
 	std::cout << "Name : "  << player.Name
-			  << ", HP : "  << player.HP
-			  << ", MP : "  << player.MP
-			  << ", ATK : " << player.ATK
-			  << ", DEF : " << player.DEF
-			  << ", SPD : " << player.SPD
+			  << ", HP : "  << player.Settings.HP
+			  << ", MP : "  << player.Settings.MP
+			  << ", ATK : " << player.Settings.ATK
+			  << ", DEF : " << player.Settings.DEF
+			  << ", SPD : " << player.Settings.SPD
 			  << std::endl;
 }
 
 int main()
 {
-	Player player1("勇者", 200, 0, 10, 5, 1);
-	Player player2 = player1;
-	Player player3("勇者", hero);
+	Player player("勇者", hero);
 
-	Show(player1);
-	Show(player2);
-	Show(player3);
+	Show(player);
 }
