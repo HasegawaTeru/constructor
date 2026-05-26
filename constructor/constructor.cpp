b66d48734b03d3f8707a869343c65fc3da9ef536
4D-12\koyo <a>
2026-05-26 14:41:25 +0900

    構造体追加

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -1,27 +1,67 @@
 ﻿#include <iostream>
 
+struct PlayerSettings
+{
+	int HP;
+	int MP;
+	int ATK;
+	int DEF;
+	int SPD;
+};
+
+PlayerSettings hero = {
+	1000, 100, 100, 50, 10
+};
+
 class Player
 {
 public:
 	std::string Name;
 	int HP;
+	int MP;
+	int ATK;
+	int DEF;
+	int SPD;
 
 	Player()
-		: Player("Default", 100)
+		: Player("Default")
 	{
 	}
 
-	Player(std::string name, int hp)
-		: Name(name), HP(hp)
+	Player(std::string name, int hp = 100, int mp = 0, int atk = 1, int def = 1, int spd = 1)
+		: Name(name), HP(hp), MP(mp), ATK(atk), DEF(def), SPD(spd)
+	{
+	}
+
+	Player(const Player& other)
+		: Name(other.Name), HP(other.HP), MP(other.MP), ATK(other.ATK), DEF(other.DEF), SPD(other.SPD)
+	{
+	}
+
+	Player(std::string name, const PlayerSettings& setting)
+		: Player(name, setting.HP, setting.MP, setting.ATK, setting.DEF, setting.SPD)
 	{
 	}
 };
 
+void Show(const Player& player)
+{
+	std::cout << "Name : "  << player.Name
+			  << ", HP : "  << player.HP
+			  << ", MP : "  << player.MP
+			  << ", ATK : " << player.ATK
+			  << ", DEF : " << player.DEF
+			  << ", SPD : " << player.SPD
+			  << std::endl;
+}
+
 int main()
 {
-	Player player1;
-	Player player2("勇者", 200);
+	Player player1("勇者", 200, 0, 10, 5, 1);
+	Player player2 = player1;
+	Player player3("勇者", hero);
 
-	std::cout << "Name : " << player1.Name << ", HP : " << player1.HP << std::endl;
-	std::cout << "Name : " << player2.Name << ", HP : " << player2.HP << std::endl;
+	Show(player1);
+	Show(player2);
+	Show(player3);
 }
